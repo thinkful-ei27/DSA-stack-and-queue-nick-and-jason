a2975e8876901e0bd9bdb1823a5efbde26e6f24c
nickJ <nicknotjustin@gmail.com>
2019-03-01 11:06:12 -0600

    is_palindrome works. Takes in a series of characters and returns whether the series is a palindrome after removing spaces and puncutations.

--- DSA-Stack-and-Queue.js
+++ DSA-Stack-and-Queue.js
@@ -51,14 +51,42 @@ const display = stack => {
   }
 }
 
+//Palindromes
+function is_palindrome(input){
+  input = input.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  const test1 = new Stack();
+  const test2 = new Stack();
+  for(let x = 0; x < input.length; x++){
+    test1.push(input.charAt(x));
+    test2.push(input.charAt(input.length - 1 - x))
+  }
+  let node1 = test1.top;
+  let node2 = test2.top;
+  while(node1 && node2){
+    if(node1.data !== node2.data){
+      return false;
+    }
+    node1 = node1.next;
+    node2 = node2.next;
+  }
+  return true;
+
+}
+
+
 function main(){
   const starTrek = new Stack();
   starTrek.push('Kirk');
   starTrek.push('Spock');
   starTrek.push('McCoy');
   starTrek.push('Scotty');
-  console.log(peek(starTrek));
-  display(starTrek);
+  // console.log(peek(starTrek));
+  // display(starTrek);
+  starTrek.pop();
+  //Have to pop twice as McCoy is not on top
+  starTrek.pop();
+  // display(starTrek);
+  console.log(is_palindrome(`Madam I'm Adam`));
 }
 
 main();
